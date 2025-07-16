bb9c40df5cdd047962b32013ceb8501b668a9e6b
SammySemicolon <malummoddevelopment@gmail.com>
2025-07-16 23:59:15 +0200

    Cave carrot & Stuffed Squid Loot

--- src/main/java/com/sammy/minersdelight/datagen/MDBlockLootTables.java
+++ src/main/java/com/sammy/minersdelight/datagen/MDBlockLootTables.java
@@ -1,13 +1,29 @@
 package com.sammy.minersdelight.datagen;
 
+import com.sammy.minersdelight.content.block.CaveCarrotBlock;
+import com.sammy.minersdelight.content.block.StuffedSquidFeastBlock;
+import com.sammy.minersdelight.setup.MDBlocks;
+import com.sammy.minersdelight.setup.MDItems;
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.core.*;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.data.*;
 import net.minecraft.data.loot.*;
 import net.minecraft.resources.*;
 import net.minecraft.world.flag.*;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.storage.loot.*;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.CopyComponentsFunction;
+import net.minecraft.world.level.storage.loot.functions.CopyNameFunction;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.parameters.*;
+import net.minecraft.world.level.storage.loot.predicates.ExplosionCondition;
+import net.minecraft.world.level.storage.loot.predicates.InvertedLootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import net.neoforged.neoforge.registries.*;
 
 import java.util.*;
@@ -44,15 +60,48 @@ public class MDBlockLootTables extends LootTableProvider {
 
             take(blocks, COPPER_POT);
             take(blocks, STICKY_BASKET);
-            take(blocks, WILD_CAVE_CARROTS);
-            take(blocks, STUFFED_SQUID);
             take(blocks, GLAZED_ARACHNID_LIMBS);
 
-            takeAll(blocks, b -> b.get() instanceof FlowerPotBlock).forEach(b -> add(b.get(), createPotFlowerItemTable(((FlowerPotBlock)b.get()).getPotted())));
+            createStuffedSquidTable(take(blocks, STUFFED_SQUID).get());
+            createCaveCarrotCropTable(take(blocks, CAVE_CARROTS).get());
+
+            takeAll(blocks, b -> b.get() instanceof FlowerPotBlock).forEach(b -> add(b.get(), createPotFlowerItemTable(((FlowerPotBlock) b.get()).getPotted())));
 
             takeAll(blocks, b -> true).forEach(b -> add(b.get(), createSingleItemTable(b.get().asItem())));
         }
 
+
+        protected void createStuffedSquidTable(Block block) {
+            LootTable.Builder builder = LootTable.lootTable();
+            LootPool.Builder fullBlock = LootPool.lootPool().when(ExplosionCondition.survivesExplosion())
+                    .add(LootItem.lootTableItem(MDBlocks.STUFFED_SQUID.get().asItem())
+                            .when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
+                                    .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(StuffedSquidFeastBlock.SERVINGS, 5)))
+                            .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1))));
+            LootPool.Builder bowl = LootPool.lootPool().when(ExplosionCondition.survivesExplosion())
+                    .add(LootItem.lootTableItem(Items.BOWL)
+                            .when(InvertedLootItemCondition.invert(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
+                                    .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(StuffedSquidFeastBlock.SERVINGS, 5))))
+                            .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1))));
+            add(block, builder.withPool(fullBlock).withPool(bowl));
+        }
+
+        protected void createCaveCarrotCropTable(Block block) {
+            LootTable.Builder builder = LootTable.lootTable();
+            LootPool.Builder fullyGrown = LootPool.lootPool().when(ExplosionCondition.survivesExplosion())
+                    .add(LootItem.lootTableItem(MDItems.CAVE_CARROT.get())
+                            .when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
+                                    .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(CaveCarrotBlock.AGE, 3)))
+                            .apply(SetItemCountFunction.setCount(UniformGenerator.between(2, 4))));
+            LootPool.Builder notFullyGrown = LootPool.lootPool().when(ExplosionCondition.survivesExplosion())
+                    .add(LootItem.lootTableItem(MDItems.CAVE_CARROT.get())
+                            .when(InvertedLootItemCondition.invert(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
+                                    .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(CaveCarrotBlock.AGE, 3))))
+                            .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1))));
+            add(block, builder.withPool(fullyGrown).withPool(notFullyGrown));
+        }
+
+        //This removes the error that's thrown if some blocks lack a loot table generated via datagen
         @Override
         public void generate(BiConsumer<ResourceKey<LootTable>, LootTable.Builder> output) {
             this.generate();
