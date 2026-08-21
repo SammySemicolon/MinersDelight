abd6e3c32d1eeb7512c4f0a0febee1b114a49df7
Sam2451 <134775965+Sam2451@users.noreply.github.com>
2026-08-21 21:24:18 +0200

    Update CopperCupItem.java

--- src/main/java/com/sammy/minersdelight/content/item/CopperCupItem.java
+++ src/main/java/com/sammy/minersdelight/content/item/CopperCupItem.java
@@ -190,7 +190,7 @@ public class CopperCupItem extends Item implements DispensibleContainerItem {
    @Override
    public net.minecraftforge.common.capabilities.ICapabilityProvider initCapabilities(ItemStack stack, @Nullable net.minecraft.nbt.CompoundTag nbt) {
       if (this.getClass() == CopperCupItem.class)
-         return new net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper(stack);
+         return new com.sammy.minersdelight.data.CopperCupContainer(stack);
       else
          return super.initCapabilities(stack, nbt);
    }
